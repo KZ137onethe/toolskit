0fa0a6c298f05394aa2ffbb806c2ac328b431d50
guanhai <q2274799107@126.com>
2026-07-16 22:52:46 +0800

    feat(browser): 注入全局sass样式

--- playground/browser/rsbuild.config.js
+++ playground/browser/rsbuild.config.js
@@ -1,26 +1,30 @@
 // @ts-check
 import { defineConfig } from "@rsbuild/core";
-import { pluginSass } from '@rsbuild/plugin-sass';
+import { pluginSass } from "@rsbuild/plugin-sass";
+
+import { join } from "node:path";
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  root: join(__dirname, "pages"),
   source: {
-		entry: {
-			"main": "./main.js",
-			"best": "./pages/best/index.js",
-			"codec": "./pages/codec/index.js"
-		}
-	},
-	html: {
-		template({ entryName }) {
-			return ({
-				"main": "./index.html",
-				"best": "./pages/best/index.html",
-				"codec": "./pages/codec/index.html"
-			})[entryName] || "./index.html"
-		}
-	},
-	plugins: [
-		pluginSass()
-	]
+    entry: {
+      main: "./main.js",
+      best: "./best/index.js",
+      codec: "./codec/index.js",
+    },
+    preEntry: join(__dirname, "./styles/global.scss"),
+  },
+  html: {
+    template({ entryName }) {
+      return (
+        {
+          main: "./index.html",
+          best: "./best/index.html",
+          codec: "./codec/index.html",
+        }[entryName] || "./index.html"
+      );
+    },
+  },
+  plugins: [pluginSass()],
 });
