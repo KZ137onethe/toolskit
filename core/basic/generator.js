32fd71427f65f1b3d952f886ff6de4f4926edade
guanhai <q2274799107@126.com>
2026-07-20 16:00:58 +0800

    feat(core): 补充自定义的生成器方法

---
+++ core/basic/generator.js
@@ -0,0 +1,26 @@
+/**
+ * 生成start~end递增的数组，包头不包尾
+ * @param {number} start 起始值
+ * @param {number} end 终止值
+ * @returns {Generator<number>}
+ */
+function* range(start, end) {
+  while (end > start) {
+    yield start++;
+  }
+}
+
+/**
+ * 生成一个以start开头, step为步长，长度为len的数字
+ * @param {number} start
+ * @param {number} len
+ * @param {number} step
+ */
+function* generateArray(start, len, step = 1) {
+  while (len--) {
+    yield start;
+    start += step;
+  }
+}
+
+export { range, generateArray };
