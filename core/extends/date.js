401f94bc7b5ff8b4c1ca7aabf84e13d2eecc514c
guanhai <q2274799107@126.com>
2026-07-20 16:00:31 +0800

    feat(core): 补充自定义DateEx类的添加时间方法，减少时间方法，是否是闰年，是否在比较日期前，是否在比较日期后，日期是否相同这六个方法

--- core/extends/date.js
+++ core/extends/date.js
@@ -1,20 +1,72 @@
 // 自定义日期方法, 参考 day.js，文档：https://day.js.org/zh-CN/
-
-class UseDate extends Date {
+class DateEx extends Date {
+  /**
+   * 创建 DateEx 实例
+   * @param  {...any} args 初始化实例传入的初始值，同Date实例一致
+   */
   constructor(...args) {
     super(...args);
     this.timestamp = this.valueOf();
   }
 
   // 操作类
-  // 返回增加一定时间的Date对象
-  add() {}
+  /**
+   * 返回增加一定时间的DateEx对象
+   * @param {number} num 增加的单位数字
+   * @param { 'second' | 'minute' | 'hour' | 'day' | 'month' | 'year'} unit 增加的单位
+   * @returns {DateEx}
+   */
+  add(num, unit) {
+    let nowTimestamp = 0;
+		const date = new Date(this.timestamp);
+    switch (unit) {
+      case "year": {
+        nowTimestamp = date.setFullYear(date.getFullYear() + num);
+        break;
+      }
+      case "month": {
+        nowTimestamp = date.setMonth(date.getMonth() + num);
+        break;
+      }
+      case "day": {
+        nowTimestamp = date.setDate(date.getDate() + num);
+        break;
+      }
+      case "hour": {
+        nowTimestamp = date.setHours(date.getHours() + num);
+        break;
+      }
+      case "minute": {
+        nowTimestamp = date.setMinutes(date.getMinutes() + num);
+        break;
+      }
+      case "second": {
+        nowTimestamp = date.setSeconds(date.getSeconds() + num);
+        break;
+      }
+      default: {
+        throw new Error(`不支持的单位: ${unit}`);
+      }
+    }
+    return new DateEx(nowTimestamp);
+  }
 
-  // 返回减去一定时间的Date对象
-  subtract() {}
+  /**
+   * 返回减去一定时间的Date对象
+   * @param {number} num 减少的单位数字
+   * @param {'second' | 'minute' | 'hour' | 'day' | 'month' | 'year'} unit 减少的单位
+   * @returns {DateEx}
+   */
+  subtract(num, unit) {
+    return this.add(-num, unit);
+  }
 
   // 显示类
-  // 根据传入的占位符返回格式化后的日期
+  /**
+   * 根据传入的占位符返回格式化后的日期
+   * @param {string | undefined} formatStr 格式化字符串，不填则使用默认的
+   * @returns {string} 格式化后的日期
+   */
   format(formatStr) {
     if (formatStr === undefined) {
       const opts = {
@@ -44,20 +96,61 @@ class UseDate extends Date {
     });
   }
 
+  // 是否是闰年
+  isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  }
+
   // 比较类
-  // 时间是否相同
-  isSame() {}
+  /**
+   * DateEx 对象是否在另一个提供的日期时间之前。
+   * @param {Date | DateEx} date 比较对象
+   * @param {'second' | 'minute' | 'hour' | 'day' | 'month' | 'year'} unit 比较单位
+   */
+  isBefore(date, unit = undefined) {
+    // 直接比较Date对象的时间戳
+    if (unit === undefined) {
+      return this.timestamp < date.valueOf();
+    }
+    // 当传入单位的时候要判断比较对象是否小于单位范围的最大值
+    let end = this.add(1, unit).setMilliseconds(0);
+    return date.valueOf() < end;
+  }
 
   /**
-   * 对时间进行比较
-   * @param {Date} date 时间
-   * @param {*} options
-   * @property {'before' | 'after'} options.way
-   * @property {}
+   * DateEx 对象是否在另一个提供的日期时间之后。
+   * @param { Date | DateEx } date 比较对象
+   * @param {'second' | 'minute' | 'hour' | 'day' | 'month' | 'year'} unit 比较单位
    */
-  compare(date, options = { way, unit }) {}
+  isAfter(date, unit = undefined) {
+    // 直接比较Date对象的时间戳
+    if (unit === undefined) {
+      return this.timestamp > date.valueOf();
+    }
+    // 当传入单位的时候要判断比较对象是否大于单位范围的最大值
+    let end = this.add(1, unit).setMilliseconds(0);
+    return date.valueOf() > end;
+  }
+
+  /**
+   * 时间是否相同
+   * @param { Date | DateEx } date 比较对象
+   * @param {'second' | 'minute' | 'hour' | 'day' | 'month' | 'year'} unit 比较单位
+   */
+  isSame(date, unit = undefined) {
+    // 直接比较Date对象的时间戳
+    if (unit === undefined) {
+      return this.timestamp === date.valueOf();
+    }
+    // 当传入单位的时候要判断比较对象是否大于单位范围的最大值
+    let start = this.setMilliseconds(0);
+    let end = this.add(1, unit).setMilliseconds(0);
+    return date.valueOf() > start && date.valueOf() < end;
+  }
 }
 
-const d = new UseDate();
+const d = new DateEx();
 console.log(d.format("YYYY-MM-DD HH:mm:ss"));
-console.log(d.format());
+console.log(d.add(2, "year").subtract(2, "month").format());
+console.log(d.format("YYYY-MM-DD HH:mm:ss"));
+console.log(d.isSame(new Date(), "minute"));
